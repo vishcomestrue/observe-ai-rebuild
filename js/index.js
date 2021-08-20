4ded8fd54285cd54ca9f033c87e650d3be7ffda9
vishcomestrue <ramvishwa03@gmail.com>
2021-08-20 23:52:12 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,4 +2,19 @@ window.addEventListener('DOMContentLoaded', () => {
   let products = document.getElementById('products')
   let resources = document.getElementById('resources')
   let company = document.getElementById('company')
+
+  window.onscroll = function() {checkScroll()};
+
+  let navbar = document.getElementById("navbar");
+
+  let sticky = navbar.offsetTop;
+
+  function checkScroll() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } 
+    else {
+      navbar.classList.remove("sticky");
+    }
+  } 
 })
