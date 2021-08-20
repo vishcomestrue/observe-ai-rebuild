7c5f3fcf0488538f4750d96716f5b0fc02e239be
vishcomestrue <ramvishwa03@gmail.com>
2021-08-20 23:08:04 +0530

    Added JS file

---
+++ js/index.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  let products = document.getElementById('products')
+  let resources = document.getElementById('resources')
+  let company = document.getElementById('company')
+})
