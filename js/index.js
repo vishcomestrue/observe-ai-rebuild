87d3f25fcd54982464da7a4e8c5bbc3a68e2849f
vishcomestrue <ramvishwa03@gmail.com>
2021-08-22 12:34:13 +0530

    Aligned the content after removed cookie box

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@ window.addEventListener('DOMContentLoaded', () => {
   
   let button = document.getElementById('cookie')
   let element = document.getElementById('cookieConsent')
+  let main = document.getElementById('main')
 
   button.addEventListener('click', () => {
     element.classList.add('hide')
     console.log("hidden")
+    main.style.marginTop = "0px"
   })
   
   let products = document.getElementById('products')
