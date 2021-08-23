ac814a441851bc4b81b238883be05e27424803e5
vishcomestrue <ramvishwa03@gmail.com>
2021-08-24 00:38:43 +0530

    Added functionalities for next and previous buttons

--- js/index.js
+++ js/index.js
@@ -24,4 +24,54 @@ window.addEventListener('DOMContentLoaded', () => {
       navbar.classList.remove("sticky");
     }
   } 
+
+  // CAROUSEL SETTINGS!
+
+  const container = document.querySelector('.fourthContainer__carousel--content-container')
+  const slides = Array.from(container.children)
+
+  const nextBtn = document.querySelector('.fourthContainer__carousel--button-right')
+  const prevBtn = document.querySelector('.fourthContainer__carousel--button-left')
+
+  const dotNavbar = document.querySelector('.fourthContainer__carousel--content-nav')
+  const dots = Array.from(dotNavbar.children)
+
+
+
+  //If previous button is pressed move one slide front. If we are in first slide move to last slide
+
+  //If next button is pressed move one slide next. If we are in last slide move to first slide
+
+  const setSlideDisplay = (currentSlide, targetSlide) => {
+    currentSlide.classList.remove('carousel--slide-active')
+    targetSlide.classList.add('carousel--slide-active')
+  }
+
+  nextBtn.addEventListener('click', e => {
+    const currentSlide = container.querySelector('.carousel--slide-active')
+
+    if(currentSlide == slides[4]) {
+      const targetSlide = slides[0]
+      setSlideDisplay(currentSlide, targetSlide)
+    }
+
+    else {
+      const targetSlide = currentSlide.nextElementSibling
+      setSlideDisplay(currentSlide, targetSlide)
+    }
+  })
+
+  prevBtn.addEventListener('click', e => {
+    const currentSlide = container.querySelector('.carousel--slide-active')
+
+    if(currentSlide == slides[0]) {
+      const targetSlide = slides[4]
+      setSlideDisplay(currentSlide, targetSlide)
+    }
+
+    else {
+      const targetSlide = currentSlide.previousElementSibling
+      setSlideDisplay(currentSlide, targetSlide)
+    }
+  })
 })
