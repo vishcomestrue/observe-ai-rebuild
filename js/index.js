4178831a1b17525bbd1cff9d609dad8495683147
vishcomestrue <ramvishwa03@gmail.com>
2021-08-24 16:35:08 +0530

    Added JS for carousel to work

--- js/index.js
+++ js/index.js
@@ -47,31 +47,71 @@ window.addEventListener('DOMContentLoaded', () => {
     targetSlide.classList.add('carousel--slide-active')
   }
 
+  const setDotDisplay = (currentDot, targetDot) => {
+    currentDot.classList.remove('carousel__active')
+    targetDot.classList.add('carousel__active')
+  }
+
   nextBtn.addEventListener('click', e => {
     const currentSlide = container.querySelector('.carousel--slide-active')
+    const currentDot = dotNavbar.querySelector('.carousel__active')
 
-    if(currentSlide == slides[4]) {
+    if(currentSlide === slides[4]) {
       const targetSlide = slides[0]
       setSlideDisplay(currentSlide, targetSlide)
+      
+      const targetDot = dots[0]
+      setDotDisplay(currentDot, targetDot)
     }
 
     else {
       const targetSlide = currentSlide.nextElementSibling
       setSlideDisplay(currentSlide, targetSlide)
+
+      const indexOfSlide = (slide) => slide === targetSlide
+      const targetIndex = slides.findIndex(indexOfSlide)
+      const targetDot = dots[targetIndex]
+      setDotDisplay(currentDot, targetDot)
     }
   })
 
   prevBtn.addEventListener('click', e => {
     const currentSlide = container.querySelector('.carousel--slide-active')
+    const currentDot = dotNavbar.querySelector('.carousel__active')
 
-    if(currentSlide == slides[0]) {
+    if(currentSlide === slides[0]) {
       const targetSlide = slides[4]
       setSlideDisplay(currentSlide, targetSlide)
+
+      const targetDot = dots[4]
+      setDotDisplay(currentDot, targetDot)
     }
 
     else {
       const targetSlide = currentSlide.previousElementSibling
       setSlideDisplay(currentSlide, targetSlide)
+      
+      const indexOfSlide = (slide) => slide === targetSlide
+      const targetIndex = slides.findIndex(indexOfSlide)
+      const targetDot = dots[targetIndex]
+      setDotDisplay(currentDot, targetDot)
     }
+  })
+
+  dotNavbar.addEventListener('click', e => {
+    const targetDot = e.target.closest('button')
+
+    if(!targetDot) return;
+
+    const currentSlide = container.querySelector('.carousel--slide-active')
+    const currentDot = dotNavbar.querySelector('.carousel__active')
+
+
+    const indexOfDot = (dot) => dot === targetDot
+    const targetIndex = dots.findIndex(indexOfDot)
+    const targetSlide = slides[targetIndex]
+
+    setSlideDisplay(currentSlide, targetSlide)
+    setDotDisplay(currentDot, targetDot)
   })
 })
